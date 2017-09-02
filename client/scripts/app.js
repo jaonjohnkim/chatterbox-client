95640629c80b72a2dfff7bee2e2d4827be8e378b
jaonjohnkim <kim.jaon79@gmail.com>
2017-09-01 17:53:00 -0700

    Passed all specs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,16 +1,27 @@
-class App {
+class App { // if subclass, need 'extends Superclass'
   constructor () {
-    this.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages'
+    // if subclass, we need super();
+    // define properties
+    // format: this.propertyName = property;
+    // this.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
+    this.server = 'http://parse.sfm6.hackreactor.com/';
+    this.init();
   }
+  
+  // methodName () {
+  //   this.value = newValue;  
+  // }
   init () {
 
+
+    $('#main .username').click(this.handleUsernameClick);
+    $('#send .submit').submit(this.handleSubmit);
   }
   send (message) {
     $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      url: this.server,
       type: 'POST',
-      data: JSON.stringify(message),
+      data: message,
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
@@ -21,20 +32,71 @@ class App {
       }
     });
   }
+
   fetch () {
     $.ajax({
-      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      url: this.server,
       type: 'GET',
-      contentType: 'applciation/json',
+      contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message fetched');
+        console.log('chatterbox: Data fetched');
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to fetch message', data);
+        console.error('chatterbox: Failed to fetch data', data);
       }
     });
   }
+  clearMessages () {
+    $.ajax({
+      url: this.server,
+      type: 'DELETE',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(data);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to delete message', data);
+      }
+    });
+    $('#chats').children().remove();
+  }
+  renderMessage(message) {
+    $.ajax({
+      url: this.server,
+      type: 'POST',
+      contentType: 'application/json',
+      success: function (data) {
+        
+        console.log('chatterbox: message rendered');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to render message', data);
+      }
+    });
+    
+    var userName = $('<span />').attr('class', 'username').html(message.username);
+    var roomName = $('<span />').attr('class', 'roomname').html(message.roomname);
+    var text = $('<div />').attr('class', 'text').html(message.text);
+    var header = $('<div />').append(userName).append('@').append(roomName).append(' : ').append(text);
+    header.appendTo($('#chats'));
+    
+
+  }
+  renderRoom(name) {
+    var option = document.createElement('option');
+    option.text = name;
+    var room = document.getElementById('roomSelect');
+    var newRoom = room.add(option);
+  }
+  handleUsernameClick() {
+    console.log(this);
+  }
+  handleSubmit() {
+    console.log(this);
+  }  
 }
 
 
